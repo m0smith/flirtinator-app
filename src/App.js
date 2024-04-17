0d9924570521478d094989c9ac7836b8df36ea25
Matthew O. Smith <matt@m0smith.com>
2024-04-17 08:51:38 -0600

    Getting started

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Button, Container, TextField, Typography, Select, MenuItem } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Typography variant="h2" component="h1" gutterBottom>
+        Flirtinator
+      </Typography>
+      <Select defaultValue="" fullWidth>
+        <MenuItem value="Love">Love</MenuItem>
+        <MenuItem value="Compliment">Compliment</MenuItem>
+      </Select>
+      <TextField fullWidth label="Enter Name" margin="normal" />
+      <Button variant="contained" color="primary">
+        Generate Image
+      </Button>
+    </Container>
   );
 }
 
